6d887b44fb77fdb7356ba1dfe48c191835ca3563
Reynan Kanindot <ekahiyang@gmail.com>
2022-04-25 13:26:54 +1000

    trying background images from import

--- src/components/pages/CodeCard.js
+++ src/components/pages/CodeCard.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { CodeItems } from './CodeItems';
+import CodeItems from './CodeItems';
 import Image from '../assets/images/react.png'
-
-
 
 
 
@@ -10,25 +8,37 @@ class CodeCard extends Component {
 
     render() {
         return (
-            <div className='container' style={{height: "500px"}}>
-                <div className='row row-cols-3 ' style={{height: "400px"}}>
+            <div className='container' style={{ height: "500px" }}>
+                <div className='row row-cols-3 ' style={{ height: "400px" }}>
                     {
                         CodeItems.map((item, index) => {
 
-                            
-                            const divStyle = {
-                               
-                                backgroundImage: 'url(' + Image + ')',
-                              };
-                    
+
+                            // console.log(item.myBackground);
+
+                            const backImage = item.myBackground;
+                            console.log(backImage);
+
+
+
+                            var sectionStyle = {
+                                width: "100%",
+                                height: "400px",
+                                backgroundImage: "url(" + item.myBackground + ")"
+                                // backgroundImage: bckImg
+                            };
+
                             return (
 
-                                <div className='col box h-50 text-center bckImg' style={{height: "200px",  backgroundImage: divStyle}} >
-                                    
-                                    <div key={index} >
+                                // <div className='col box h-50 text-center' style={sectionStyle}>
+                                <div className='col box h-50 text-center' >
+
+                                    <div key={index} style={sectionStyle} >
                                         <a href={item.applink} target="_blank" rel="noreferrer">{item.title}</a>
                                         <a href={item.githublink} target="_blank" rel="noreferrer"><i className="fa-brands fa-github-square fa-2xl px-3"></i></a>
+                                        <img src={item.myBackground} className="logo" alt='' />
                                     </div>
+
                                 </div>
 
                             )
--- src/components/pages/CodeItems.js
+++ src/components/pages/CodeItems.js
@@ -1,43 +1,44 @@
 
-
-
-export const CodeItems = [
+const CodeItems = 
+[
     {
         title: 'Web-DevOp-Quiz',
         githublink: 'https://github.com/Eugene32/Web-DevOp-Quiz',
         applink: 'https://eugene32.github.io/Web-DevOp-Quiz/',
-        myBackground: "../assets/images/react.png"
+        myBackground: "../assets/images/jscript-image.jpeg"
 
     },
     {
         title: 'Weather Dashboard',
         githublink: 'https://github.com/Eugene32/Weather-Dashboard',
         applink: 'https://eugene32.github.io/Weather-Dashboard/',
-        myBackground: '../assets/images/react.png'
+        myBackground: "../assets/images/web-api-tutorial"
     },
     {
         title: 'Password Generator',
         githublink: 'https://github.com/Eugene32/Password-Generator',
         applink: 'https://eugene32.github.io/Password-Generator/',
-        myBackground: 'url("../assets/images/jscript-image.jpeg")'
+        myBackground: "../assets/images/nav-backgroud.jpg"
     },
     {
         title: 'MVC Tech-Blog',
         githublink: 'https://github.com/Eugene32/MVC-Tech-Blog',
         applink: 'https://mvc-tech-blog-rey.herokuapp.com/',
-        myBackground: 'url("../assets/images/jscript-image.jpeg")'
+        myBackground: "../assets/images/SQL-image.jpeg"
     },
     {
         title: 'ECommerce Backend',
         githublink: 'https://github.com/Eugene32/ECommerce-Back-End',
         applink: 'https://drive.google.com/file/d/1q4l_tl_GvmR4kw3qaUc51PIPfPwQayCi/view',
-        myBackground: 'url("../assets/images/jscript-image.jpeg")'
+        myBackground: "../assets/images/SQL-image.jpeg"
     },
     {
         title: 'Social Network API (Mongodb)',
         githublink: 'https://github.com/Eugene32/Social-Network-API',
         applink: 'https://drive.google.com/file/d/1v-e1Yybqx7KxnCljpmiPk92psB3gJTKs/view',
-        myBackground: 'url("../assets/images/jscript-image.jpeg")'
+        myBackground: "../assets/images/SQL-image.jpeg"
     },
 
 ]
+
+export
